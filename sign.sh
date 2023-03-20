e368eea9522408b79aa386958a19b578d630725d
Roberto Montero <roberto.montero@commercetools.com>
2023-03-20 16:01:24 +0100

    Added release.yaml

--- sign.sh
+++ sign.sh
@@ -5,8 +5,6 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 set -x
 rm -f gosu*.asc SHA256SUMS.asc
-gpg --batch --gen-key genkey-batch
-gpg --list-keys
 for f in gosu*; do
 	gpg --output "$f.asc" --detach-sign "$f"
 done
